ee4a159586ec5bc55f7306e52f0b630f4139638d
suyh <su_787910081@sina.com>
2018-10-22 23:45:02 +0800

    DEMO

---
+++ CsharpDemo/SslClientDemo/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SslClientDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                TcpClient client = new TcpClient("127.0.0.1", 6000);
+                Console.WriteLine("Client connected.");
+                _sslStream = new SslStream(
+                   client.GetStream(),
+                   false,
+                   new RemoteCertificateValidationCallback(ValidateServerCertificate),
+                   null
+                   );
+
+
+                X509CertificateCollection certs = new X509CertificateCollection();
+                X509Certificate cert = X509Certificate.CreateFromCertFile(System.Environment.CurrentDirectory + @"\" + "client.cer");
+                certs.Add(cert);
+                //验证证书
+                try
+                {
+                    _sslStream.AuthenticateAsClient("test", certs, SslProtocols.Tls, false);
+                }
+                catch (AuthenticationException )
+                {
+                    client.Close();
+
+                    //Console.WriteLine("Exception: {0}", e.Message);
+                    //if (e.InnerException != null)
+                    //{
+                    //    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                    //}
+                    //Console.WriteLine("Authentication failed - closing the connection.");
+                    //client.Close();
+                    //Console.ReadLine();
+                    return;
+                }
+
+                //开始读取消息
+                Task.Factory.StartNew(() =>
+                {
+                    ReadMessage(_sslStream);
+                });
+
+                Console.WriteLine("按Q退出程序");
+                string message = "";
+                message = Console.ReadLine() + "<EOF>";
+                while (message != "Q")
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(message);
+                    _sslStream.Write(bytes);
+                    _sslStream.Flush();
+                    Console.WriteLine("send:" + message);
+                    message = Console.ReadLine() + "<EOF>";
+                }
+
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+                Console.ReadLine();
+            }
+        }
+
+        private static SslStream _sslStream;
+
+
+
+        public static void ReadMessage(SslStream sslStream)
+        {
+            byte[] buffer = new byte[2048];
+            StringBuilder messageData = new StringBuilder();
+            int bytes = -1;
+            do
+            {
+                bytes = sslStream.Read(buffer, 0, buffer.Length);
+                Decoder decoder = Encoding.UTF8.GetDecoder();
+                char[] chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
+                decoder.GetChars(buffer, 0, bytes, chars, 0);
+                messageData.Append(chars);
+                if (messageData.ToString().IndexOf("<EOF>", StringComparison.Ordinal) != -1)
+                {
+                    break;
+                }
+            } while (bytes != 0);
+
+            string message = messageData.ToString().Replace("<EOF>", "");
+            Console.WriteLine("recevied:" + message);
+            ReadMessage(sslStream);
+        }
+
+        private static bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslpolicyerrors)
+        {
+            if (sslpolicyerrors == SslPolicyErrors.None)
+                return true;
+            Console.WriteLine("Certificate error: {0}", sslpolicyerrors);
+            return false;
+        }
+    }
+}
---
+++ CsharpDemo/SslServerDemo/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SslServerDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 6000);
+            listener.Start();
+
+            Console.WriteLine("Waiting for a client to connect...");
+            TcpClient client = listener.AcceptTcpClient();
+
+            _sslStream = new SslStream(client.GetStream(), true);
+
+            try
+            {
+                serverCertificate = new X509Certificate(Environment.CurrentDirectory + @"\" + "server.pfx", "1");
+                _sslStream.AuthenticateAsServer(serverCertificate, false, SslProtocols.Tls, true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.ReadLine();
+                return;
+            }
+
+            while (true)
+            {
+                string receivedMessage = ReadMessage(_sslStream);
+                Console.WriteLine("received:" + receivedMessage);
+                byte[] message = Encoding.UTF8.GetBytes("Success.<EOF>");
+                _sslStream.Write(message);
+                _sslStream.Flush();
+            }
+        }
+
+        static X509Certificate serverCertificate = null;
+        private static SslStream _sslStream;
+
+
+
+        static string ReadMessage(SslStream sslStream)
+        {
+            byte[] buffer = new byte[2048];
+            StringBuilder messageData = new StringBuilder();
+            int bytes = -1;
+            do
+            {
+                bytes = sslStream.Read(buffer, 0, buffer.Length);
+                Decoder decoder = Encoding.UTF8.GetDecoder();
+                char[] chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
+                decoder.GetChars(buffer, 0, bytes, chars, 0);
+                messageData.Append(chars);
+                if (messageData.ToString().IndexOf("<EOF>") != -1)
+                {
+                    break;
+                }
+            } while (bytes != 0);
+
+            return messageData.ToString();
+        }
+
+        static void ProcessClient(TcpClient client)
+        {
+
+            SslStream sslStream = new SslStream(
+                client.GetStream(), true);
+
+            try
+            {
+                sslStream.AuthenticateAsServer(serverCertificate, true, SslProtocols.Tls, true);
+
+                Console.WriteLine("Waiting for client message...");
+                string messageData = ReadMessage(sslStream);
+                Console.WriteLine("Received: {0}", messageData);
+
+                byte[] message = Encoding.UTF8.GetBytes("已收到信息.<EOF>");
+                sslStream.Write(message);
+                sslStream.Flush();
+            }
+            catch (AuthenticationException e)
+            {
+                Console.WriteLine("Exception: {0}", e.Message);
+                if (e.InnerException != null)
+                {
+                    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                }
+                Console.WriteLine("Authentication failed - closing the connection.");
+                sslStream.Close();
+                client.Close();
+                return;
+            }
+            finally
+            {
+                sslStream.Close();
+                client.Close();
+            }
+        }
+    }
+}
